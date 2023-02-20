b8fb5ce43a467c6d091c94678bb2591af2c32e9f
faxcorp <37246339+faxcorp@users.noreply.github.com>
2023-02-20 21:37:46 +0200

    Add files via upload

---
+++ DriftIslandPlugin.h
@@ -0,0 +1,16 @@
+// f4x 2023
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleInterface.h"
+#include "CoreMinimal.h"
+
+class FDriftIslandPluginModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
